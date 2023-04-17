14cc506313de4e1f96ea539d38d9baee31d5eef2
Konstantin Danovich <KDanovich42@gmail.com>
2023-04-18 00:18:09 +0300

    minor fix to permit

--- src/utils/PermitVisit.js
+++ src/utils/PermitVisit.js
@@ -3,7 +3,7 @@ import { step2Schema, step3Schema } from '../data/AppointmentSchemas';
 export function permitVisitApp3(currentData) {
     // Has any of the data options from step 2
     return Object.entries(currentData)
-        .filter(([key, value]) => step2Schema.includes(key))
+        .filter(([key, value]) => step2Schema.includes(key) && key !== 'openedTab')
         .reduce((valid, [key, value]) => {
             return valid ? true : value !== '' && value !== null ? true : false;
         }, false);
