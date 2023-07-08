cdff13d0a9bfc9e5e9e9a6275890ceeb8f0eba58
Konstantin Danovich <KDanovich42@gmail.com>
2023-07-09 01:09:03 +0300

    change in client schema

--- server/src/models/client.js
+++ server/src/models/client.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const clientSchema = new mongoose.Schema({
     name: String,
     surname: String,
-    birthday: Number,
+    birthday: Date,
     email: { type: String, unique: true },
     telephone: { type: String, unique: true },
     password: String,
