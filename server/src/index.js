214bbca96188eefe609676a62dc701a8d2711d9f
Konstantin Danovich <KDanovich42@gmail.com>
2023-07-08 21:14:25 +0300

    changed resolved path to .env, moved index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import path from 'path';
-import app from './src/app.js';
+import app from './app.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '.env') });
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
