11f9c5854143720a494f0959f81aeb7fb49b449b
Konstantin Danovich <KDanovich42@gmail.com>
2023-07-09 01:58:26 +0300

    changed saveData error handler to MongoServerError from MongoError

--- server/src/dbRequests/saveData.js
+++ server/src/dbRequests/saveData.js
@@ -4,7 +4,7 @@ export default async function saveData(Model, newData) {
         await newDocument.save();
         console.log('Data saved successfully!');
     } catch (error) {
-        if (error.name === 'MongoError' && error.code === 11000) {
+        if (error.name === 'MongoServerError' && error.code === 11000) {
             const duplicates = Object.keys(error.keyPattern);
             const errorMessage = `Duplicate values found: ${duplicates.join(', ')}`;
             throw new Error(errorMessage);
