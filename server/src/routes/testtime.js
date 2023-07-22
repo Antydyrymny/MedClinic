e29a8c55655462cae6b396fd4171a1b1d97ee282
Konstantin Danovich <KDanovich42@gmail.com>
2023-07-22 18:20:36 +0300

    another tz

--- server/src/routes/testtime.js
+++ server/src/routes/testtime.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dayjs from 'dayjs';
 
+process.env.TZ = 'America/New_York';
 const router = express.Router();
 router.get('/', async (req, res) => {
     try {
